8479ddd1f1485d0f489b14469fabe82da1f28f7f
DotnetMob <dotnetmob@gmail.com>
2021-05-08 18:06:16 +0530

    Added Basics - 1.What is JavaScript

---
+++ 1. Basics/01. What is JavaScript.js
@@ -0,0 +1,53 @@
+var summary = `
+  - What is JavaScript ?
+  - Why you should learn it?
+  - <a href="https://bit.ly/3baViuN" target="_blank">Video Tutorial</a>
+ `;
+localStorage.setItem('topicSummary', summary);
+
+/*
+
+? What is JavaScript? 
+-JavaScript is a light-weight, interpreted or just-in-time compiled 
+ programming language with first class functions.
+
+Not going to make things harder for you by explaing 
+these language features now, We'll do that later.
+
+* Brendan Eich invented JS in 1995
+
+? Pre Requisite for Learning JS
+ - basic knowledge in HTML & CSS.
+
+Previously JavaScript was primarily used to add functionality 
+and behaviors to your website client-side. Nowadays it is being almost anywhere.
+
++ Web Design
+|-> Along with HTML and CSS
+|-> React, Angular, VueJS (clint-side frameworks)
+
++ Server Side
+|-> Nodejs.
+
++Mobile Development
+|-> React Native, Ionic
+
++Desktop App Development
+|-> Electron
+
++Internet of Things(IoT)
+|-> Cylon, Favor
+
++Artificial Intelligence
+|-> Tensorflow.js
+
+
+* JS Frameworks or libraries are used to build all types of applications - Front-End, Back-End, AI, IoT Mobile & Desktop, That's not the case with HTML and CSS (Primarily used for Web Designing). So nowadays you can't runaway from JS :). 
+
+? Java vs JavaScript
+ - JavaScript has no relation with Java.
+ - It's just the word 'java' in 'javascript'
+ - It's like car and carpet.
+
+ */
+
