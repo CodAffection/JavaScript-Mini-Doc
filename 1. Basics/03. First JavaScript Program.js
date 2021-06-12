be5105bc4a09fc4005fc1e3bb2a008e1b91cb3ae
DotnetMob <dotnetmob@gmail.com>
2021-06-12 08:42:52 +0530

    Added Basics -  4.Write First JS Program

---
+++ 1. Basics/03. First JavaScript Program.js
@@ -0,0 +1,36 @@
+let summary = `
+ - JS Developemnt Environment
+ - First Program in JS
+ - Video we'll be uploaded soon.xxx
+  `;
+localStorage.setItem('topicSummary', summary); 
+
+// Prints 'Hello World' to Developer Console of Browser
+console.log('Hello World')
+
+// Prints result of Math Operation 3+5.
+console.log(5+3)
+
+
+/*
+
+* Required Development Environment for JS
+- Tools to write js program
+any text editor of your choice or an IDE.
+From Notepad to IDEs like VS Code, Sublime, Atom etc
+
+- Run Time Environment
+There are many ways to run a js program.
+But we'll using a web browser to execute js code.
+
+- Extension
+There are some IDE Extensions helpful for fast and error free coding.
+
+* Methods to Run JS
+1. Within Developer Console of Web Browser itself
+2. Within HTML web page
+3. Using VS Code Extension : Quokka.js
+4. There are other ways like using node, we've not discussed in this course
+
+* Semicolon at the end of each JS line/ statement is optional. 
+*/
